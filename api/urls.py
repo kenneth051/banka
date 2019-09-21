1bb39f75d044b73dd32c496615d085bb4eb0ef68
kenneth051 <kjdumba@gmail.com>
2019-09-21 19:18:45 +0300

    add more models and serializers

--- api/urls.py
+++ api/urls.py
@@ -2,11 +2,22 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from api.views import UserView, ClientsView, LoginView
+from api.views import UserView, ClientsView, LoginView, AccountView, LoansView, TransactionsView
 
-router = SimpleRouter()
+class OptionalSlashRouter(SimpleRouter):
+    def __init__(self, trailing_slash="/?"):
+        self.trailing_slash = trailing_slash
+        super(SimpleRouter, self).__init__()
+
+
+router = OptionalSlashRouter()
+
 router.register("users",UserView,"users")
 router.register("clients", ClientsView,"clients")
+router.register("accounts", AccountView,"accounts")
+router.register("loans", LoansView,"loans")
+router.register("transactions", TransactionsView,"transactions")
+
 
 urlpatterns = [
     path('users/login/', LoginView.as_view()),
--- api/views.py
+++ api/views.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 
 from api.serializers.users import UserSerializer, LoginSerializer
 from api.serializers.clients import ClientSerializer
-from api.models import User, Clients
+from api.models import User, Clients, Account, Loans, Transactions
+from api.serializers.serializer_main import AccountSerializer, LoanSerializer, TransactionSerializer
 
 
 class UserView(ModelViewSet):
@@ -27,4 +28,20 @@ class ClientsView(ModelViewSet):
     permission_classes = (IsAuthenticated, )
     serializer_class = ClientSerializer
     queryset = Clients.objects.all()
+
+class AccountView(ModelViewSet):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
+
+class LoansView(ModelViewSet):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = LoanSerializer
+    queryset = Loans.objects.all()
+
+class TransactionsView(ReadOnlyModelViewSet):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = TransactionSerializer
+    queryset = Transactions.objects.all()
+
 
